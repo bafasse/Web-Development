2829c51a0b61a3c5de4204ef4f694a112ee246e8
bafasse <blakefasse@gmail.com>
2018-05-18 13:51:26 -0500

    Finished intro to Javascript

---
+++ Practice Stuff/(6) Intro to Javascript/Project/JS Stalker/JSstalker.js
@@ -0,0 +1,10 @@
+var firstName = prompt("what is your first name?");
+var lastName =  prompt("what is your last name?");
+var age =       prompt("How old are you?");
+var fullName = firstName + " " + lastName;
+
+alert("Hello, " + fullName);
+alert("You are " + age + " years old");
+
+console.log("Hello, " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
---
+++ Practice Stuff/(6) Intro to Javascript/Project/ageCalculator/script.js
@@ -0,0 +1,4 @@
+var age  = prompt("How old are you?");
+var days = age * 365.25; // to account for leap years
+
+alert("You are at least " + days + " days old");
